872685353026bbfbb24a99380f0967e1ff085682
chrishughes0411 <chrishughes0411@gmail.com>
2023-04-13 00:04:29 -0500

    working functions on js

--- functions.js
+++ functions.js
@@ -1,62 +1,96 @@
 //adding comment for the commit message
+var numero1, numero2, result;
+var option;
 
-var operation = "";
-var numero1;
-var numero2;
-function add(){
-    let num1 = document.getElementById('firstInput');
-    numero1 = num1.value;
-    num1.value = '';
-    operation = "suma";
-}
-
-function equals(){
-    console.log("llegue")
+//ibamos bien el unico error era que no puede tener el mismo nombre que el id
+//complete las otras operaciones con los que tenias
+var operations = "operation.js";
+function equalsOperation() {
     let num2 = document.getElementById('firstInput');
-    numero2 = num2.value;
-    //document.getElementById('firstInput').value = parseInt(numero1+numero2);
-    console.log(numero1);
+    numero2 = parseFloat(num2.value);
+    switch (option) {
+        case '+':
+            result = numero1 + numero2;
+            break;
+        case '-':
+            result = numero1 - numero2;
+            break;
+        case '*':
+            result = numero1 * numero2;
+            break;
+        case '/':
+            result = numero1 / numero2;
+            break;
+        case '^':
+            result = numero1 ** numero2;
+            break;
+        case 'sqrt':
+            result = numero1 ** (1 / numero2);
+            break;
+        default:
+            alert(`Perdon, se espera una operacion ${option}.`);
+    }
+    num2.value = result;
+    option = null;
 }
 
-// eventlstener en el + => leo el mismo inout 
-
-function substract(num1,num2){
-    return num1 - num2;
+function getNumber() {
+    let num1 = document.getElementById('firstInput');
+    numero1 = parseFloat(num1.value);
+    num1.value = '';
 }
 
-function divide(num1,num2){
-    return num1 / num2;
+function add() {
+    getNumber()
+    option = '+';
 }
 
-function multiply(num1,num2){
-    return num1 * num2;
+function substract() {
+    getNumber()
+    option = '-';
 }
 
-function power(num1,num2){
-    return num1**num2;
+function divide() {
+    getNumber()
+    option = '/';
 }
 
-function sqrt(num1,num2){
-    return num1**(1/num2);
+function multiply() {
+    getNumber()
+    option = '*';
 }
+
+function power() {
+    getNumber()
+    option = '^';
+}
+
+function sqrt() {
+    getNumber()
+    option = 'sqrt';
+}
+
 
 //the following are dummy numbers to check the functionality of the functions below
 
-let num = 30;
 
 var array = [];
-i=0;
+let i = 0;
 
 // similar to python order matters revNum is inside the function outside the while loop
 
-function bin(){
-    alert(2);
-    while (num!=0){
-        modu = num % 2;
-        array.push(modu);
-        num = Math.floor(num/2);
+function bin() {
+    if (document.getElementById('firstInput').value != '') {
+        let array = [];
+        getNumber();
+        while (numero1 != 0) {
+            modu = numero1 % 2;
+            array.push(modu);
+            numero1 = Math.floor(numero1 / 2);
+        }
+        result = array.reverse().join("");
+        document.getElementById('firstInput').value = result;
     }
-    revNum2 = array.reverse();
 }
 
 //remember to call the function PLEASE!!!
@@ -65,38 +99,46 @@ function bin(){
 // alert(revNum2);
 //-----------------------------------------
 
-function octal(){
-    alert(8);
-    while (num!=0){
-        modu = num % 8;
-        array.push(modu);
-        num = Math.floor(num/8);
+function octal() {
+    if (document.getElementById('firstInput').value != '') {
+        let array = [];
+        getNumber();
+        while (numero1 != 0) {
+            modu = numero1 % 8;
+            array.push(modu);
+            numero1 = Math.floor(numero1 / 8);
+        }
+        result = array.reverse().join("");
+        document.getElementById('firstInput').value = result;
     }
-    revNum8 = array.reverse();
 }
 
 // octal();
 // alert(revNum8);
 //---------------------------------------
 
-function hexa(){
-    alert(16);
-    while (num!=0){
-        modu = num % 16;
-        array.push(modu);
-        num = Math.floor(num/16);
-    }
-    revNum16 = array.reverse();
-    for (i in revNum16){
-         if (revNum16[i] == 10) {revNum16[i] = `A`;}
-         else if (revNum16[i] == 11) {(revNum16[i] = `B`);}
-         else if (revNum16[i] == 12) {(revNum16[i] = `C`);}
-         else if (revNum16[i] == 13) {(revNum16[i] = `D`);}
-         else if (revNum16[i] == 14) {(revNum16[i] = `E`);}
+function hexa() {
+    if (document.getElementById('firstInput').value != '') {
+        let array = [];
+        getNumber();
+        while (numero1 != 0) {
+            modu = numero1 % 16;
+            array.push(modu);
+            numero1 = Math.floor(numero1 / 16);
+        }
+        revNum16 = array.reverse();
+        for (i in revNum16) {
+            if (revNum16[i] == 10) { revNum16[i] = `A`; }
+            else if (revNum16[i] == 11) { (revNum16[i] = `B`); }
+            else if (revNum16[i] == 12) { (revNum16[i] = `C`); }
+            else if (revNum16[i] == 13) { (revNum16[i] = `D`); }
+            else if (revNum16[i] == 14) { (revNum16[i] = `E`); }
+        }
+        document.getElementById('firstInput').value = revNum16.join('');
     }
 }
 
 // hexa();
 // alert(revNum16);
 
-plus.addEventListener('click', alert(10));
+// plus.addEventListener('click', alert(10));
